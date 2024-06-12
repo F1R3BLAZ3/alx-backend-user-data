4715e1274e9b6767c4eafa54e885fab5659ca878
F1R3BLAZ3 <zandre.777.deboer@gmail.com>
2024-06-12 14:10:36 +0000

    task 7

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -3,9 +3,11 @@
 Basic Flask app
 """
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
 app = Flask(__name__)
+AUTH = Auth()
 
 @app.route('/', methods=['GET'])
 def welcome():
@@ -13,6 +15,20 @@ def welcome():
     GET route at the root that returns a JSON message.
     """
     return jsonify({"message": "Bienvenue"})
+
+@app.route('/users', methods=['POST'])
+def users():
+    """
+    POST route to register a user.
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    try:
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"}), 200
+    except ValueError as err:
+        return jsonify({"message": str(err)}), 400
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
