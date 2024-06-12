8fa915841f9de64cc8e0c5c29c464ae1be3d303c
F1R3BLAZ3 <zandre.777.deboer@gmail.com>
2024-06-12 14:13:16 +0000

    task 8

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -52,3 +52,21 @@ class Auth:
             hashed_password = _hash_password(password)
             user = self._db.add_user(email, hashed_password)
             return user
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """
+        Check if the login credentials are valid.
+
+        Args:
+            email (str): The user's email.
+            password (str): The user's password.
+
+        Returns:
+            bool: True if the login credentials are valid, False otherwise.
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            return bcrypt.checkpw(
+                password.encode(), user.hashed_password.encode())
+        except NoResultFound:
+            return False
