4aee5a003bcfe5d16e6ac3c7cebe72390c00ad5a
F1R3BLAZ3 <zandre.777.deboer@gmail.com>
2024-06-13 18:21:49 +0000

    task 15

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -68,5 +68,22 @@ def logout():
         return jsonify({"message": "Forbidden"}), 403
 
 
+@app.route('/profile', methods=['GET'])
+def profile():
+    """
+    GET route to fetch user profile based on session ID.
+    """
+    session_id = request.cookies.get('session_id')
+
+    if session_id:
+        user = AUTH.get_user_from_session_id(session_id)
+        if user:
+            return jsonify({"email": user.email}), 200
+        else:
+            abort(403)
+    else:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
